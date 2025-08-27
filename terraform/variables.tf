9a79027a5b954d67cb5c72173fc99a70465f23ca
arpitmeghwal <arpitmeghwalnmh@gmail.com>
2025-08-27 15:15:17 +0530

    Added comment for variables in variables.tf file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,11 @@
+# Variable for AWS Region
 variable "region" {
   description = "AWS region"
   type        = string
   default     = "ap-south-1"
 }
 
+# Variable for EKS Cluster Name
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
