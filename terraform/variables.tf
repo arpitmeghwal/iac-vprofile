902fb070233696c27ebbb822d07d65500c6aa87d
arpitmeghwal <arpitmeghwalnmh@gmail.com>
2025-08-27 16:24:34 +0530

    Staging Test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
-# Variable for AWS Region
+# Variable for AWS Region.
 variable "region" {
   description = "AWS region"
   type        = string
   default     = "ap-south-1"
 }
 
-# Variable for EKS Cluster Name
+# Variable for EKS Cluster Name.
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
